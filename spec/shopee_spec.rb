c33f2180eb75a5eba69486c3e2436c4f6f6326ec
qoo392 <qoo392@hotmail.com>
2015-10-17 11:00:30 +0000

    fix structure

--- spec/shopee_spec.rb
+++ spec/shopee_spec.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require 'webmock/minitest'
 require 'json'
 require_relative '../lib/shopee_scrap'
-require_relative '../vcr/vcr.rb'
+require_relative './vcr'
 
 CATEGORY_LIST = ['女生衣著', '男生衣著', '女生包包與配件', '男生包包與配件', '女鞋', '男鞋','3C相關', '手機平板與周邊', '居家生活',
 '娛樂、收藏', '代買代購', '服務、票券', '化妝、保養品', '嬰幼童與母親', '美食、伴手禮', '寵物', '戶外、運動',
@@ -14,7 +14,7 @@ CATEGORY_LIST.each do |e|
   TESTAMENT_ARR.push({'title' => e})
 end
 
-VCR.use_cassette('shopee', :record=>:once) do
+VCR.use_cassette('shopee') do
   scraper = ShopeeTileScrape::ShopeeTile.new
   describe 'Shopee testament using vcr' do
     it 'should return an array of string and name of categories' do  
